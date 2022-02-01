661a2597f7e800580d2767e9ba33971bcbdbd8e5
Guillaume GILBERT <ggilbert@e2r9p10.clusters.42paris.fr>
2022-02-01 16:23:27 +0100

    bug correction, check_file_extention

--- ft_check_file_extention.c
+++ ft_check_file_extention.c
@@ -6,7 +6,7 @@
 /*   By: ggilbert <ggilbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 19:53:42 by ggilbert          #+#    #+#             */
-/*   Updated: 2021/02/04 19:56:15 by ggilbert         ###   ########.fr       */
+/*   Updated: 2022/02/01 16:17:35 by ggilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_check_file_extention(char *av, const char *ext)
 	ext_len = ft_strlen(ext);
 	while (av[i] != '\0')
 		i++;
-	while (av[i] != '.' || i == 0)
+	while (av[i] != '.' && i > 0)
 		i--;
 	if (ft_strncmp(&av[i], ext, ext_len))
 		return (0);
